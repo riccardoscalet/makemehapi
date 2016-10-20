885f4ac446aad1fe2ecf3eda7c07ce301a5477c8
riccardoscalet <riccardo.scalet@gmail.com>
2016-10-20 12:22:11 +0200

    Completed exercises 09-10.

---
+++ 09_Validation.ts
@@ -0,0 +1,33 @@
+import * as hapi from "hapi";
+import * as min from "minimist";
+import * as joi from "joi";
+
+let argv = min(process.argv);
+
+let server = new hapi.Server();
+server.connection({
+    host: 'localhost',
+    port: process.argv[2] || 8080
+});
+
+server.route({
+    method: 'GET',
+    path: '/chickens/{breed}',
+    handler: getHandler,
+    config: {
+        validate: {
+            params: {
+                breed: joi.any().required()
+            }        
+        }
+    }
+})
+
+server.start((err) => {
+    console.log("Server started!");
+});
+
+
+function getHandler(request, reply) {
+
+}
---
+++ 10_ValidationWithJoi.ts
@@ -0,0 +1,43 @@
+import * as hapi from "hapi";
+import * as min from "minimist";
+import * as joi from "joi";
+
+let argv = min(process.argv);
+
+let server = new hapi.Server();
+server.connection({
+    host: 'localhost',
+    port: process.argv[2] || 8080
+});
+
+server.route({
+    method: 'POST',
+    path: '/login',
+    handler: loginHandler,
+    config: {
+        validate: {
+            payload: joi.object({
+                    isGuest: joi.bool().required(),
+                    username: joi.string().when('isGuest', {
+                        is: false,
+                        then: joi.string().required()
+                    }),
+                    accessToken: joi.string().alphanum(),
+                    password: joi.string().alphanum()
+                })
+                .nand('password', 'accessToken')
+                .options({
+                    allowUnknown: true
+                })
+        }
+    }
+})
+
+server.start((err) => {
+    console.log("Server started!");
+});
+
+
+function loginHandler(request, reply) {
+    reply("login successful");
+}
