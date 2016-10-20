fa68d6c2ea001060b81ab6b6fefda2e73d80830c
riccardoscalet <riccardo.scalet@gmail.com>
2016-10-20 17:18:49 +0200

    Completed all exercises.

---
+++ 13_Auth.ts
@@ -0,0 +1,43 @@
+import * as hapi from "hapi";
+import * as min from "minimist";
+import * as boom from "boom";
+const auth = require("hapi-auth-basic");
+
+let argv = min(process.argv);
+
+let server = new hapi.Server();
+server.connection({
+    host: 'localhost',
+    port: process.argv[2] || 8080
+});
+
+server.register(auth, function (err) {
+    if (err) throw err;
+});
+
+server.auth.strategy("simpleAuthenticationBanana", "basic", {
+    validateFunc: authenticate
+});
+
+server.route({
+    method: "GET",
+    path: "/",
+    config: {
+        auth: "simpleAuthenticationBanana",
+        handler: function (request, reply) {
+            console.log("User " + request.auth.credentials.name + " logged in.");
+            reply("Hello " + request.auth.credentials.name);
+        },
+    }
+});
+
+server.start((err) => {
+    console.log("Server started!");
+});
+
+function authenticate(request, username, password, callback) {
+    if (username == "hapi" && password == "auth") return callback(null, true, {
+        name: username
+    });
+    else return callback(null, false);
+}
